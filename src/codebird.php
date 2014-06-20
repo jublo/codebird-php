38b88fec80be0f3e5b93e07e6804a411595dd379
J.M <jm@jublo.net>
2014-06-20 18:42:05 +0200

    Set version to 2.5.0

--- src/codebird.php
+++ src/codebird.php
@@ -6,7 +6,7 @@ namespace Codebird;
  * A Twitter library in PHP.
  *
  * @package codebird
- * @version 2.5.0-rc.3-dev
+ * @version 2.5.0
  * @author Jublo Solutions <support@jublo.net>
  * @copyright 2010-2014 Jublo Solutions <support@jublo.net>
  *
@@ -117,7 +117,7 @@ class Codebird
     /**
      * The current Codebird version
      */
-    protected $_version = '2.5.0-rc.3-dev';
+    protected $_version = '2.5.0';
 
     /**
      * Request timeout
