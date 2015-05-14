8f4aae1c10562a758885b5177017fcfd8f0b366d
J.M <jm@jublo.net>
2015-05-14 19:51:27 +0200

    Set version to 2.7.0

--- src/codebird.php
+++ src/codebird.php
@@ -6,7 +6,7 @@ namespace Codebird;
  * A Twitter library in PHP.
  *
  * @package   codebird
- * @version   3.0.0-dev
+ * @version   2.7.0
  * @author    Jublo Solutions <support@jublo.net>
  * @copyright 2010-2015 Jublo Solutions <support@jublo.net>
  * @license   http://opensource.org/licenses/GPL-3.0 GNU General Public License 3.0
@@ -104,7 +104,7 @@ class Codebird
     /**
      * The current Codebird version
      */
-    protected $_version = '3.0.0-dev';
+    protected $_version = '2.7.0';
 
     /**
      * Auto-detect cURL absence
