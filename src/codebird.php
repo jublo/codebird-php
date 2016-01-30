ed00fe6cb065f19d64cd2ff23deab2223eb3938b
J.M <jm@jublo.net>
2016-01-30 13:53:31 +0100

    Set version to 3.1.0-rc.1

--- src/codebird.php
+++ src/codebird.php
@@ -6,7 +6,7 @@ namespace Codebird;
  * A Twitter library in PHP.
  *
  * @package   codebird
- * @version   3.1.0-dev
+ * @version   3.1.0-rc.1
  * @author    Jublo Solutions <support@jublo.net>
  * @copyright 2010-2016 Jublo Solutions <support@jublo.net>
  * @license   https://opensource.org/licenses/GPL-3.0 GNU General Public License 3.0
@@ -508,7 +508,7 @@ class Codebird
   /**
    * The current Codebird version
    */
-  protected static $_version = '3.1.0-dev';
+  protected static $_version = '3.1.0-rc.1';
 
   /**
    * The Request or access token. Used to sign requests
