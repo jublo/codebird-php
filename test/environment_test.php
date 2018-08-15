40dd1338fb64be9b52205cf275823fdcf3c3f3fe
J.M <jm@jublo.net>
2018-08-16 01:48:22 +0200

    Update tests

--- test/environment_test.php
+++ test/environment_test.php
@@ -6,8 +6,8 @@ namespace Codebird;
  * A Twitter library in PHP.
  *
  * @package   codebird-test
- * @author    Jublo Solutions <support@jublo.net>
- * @copyright 2010-2016 Jublo Solutions <support@jublo.net>
+ * @author    Jublo Limited <support@jublo.net>
+ * @copyright 2010-2018 Jublo Limited <support@jublo.net>
  * @license   https://opensource.org/licenses/GPL-3.0 GNU General Public License 3.0
  * @link      https://github.com/jublonet/codebird-php
  */
@@ -17,7 +17,7 @@ namespace Codebird;
  *
  * @package codebird-test
  */
-class Environment_Test extends \PHPUnit_Framework_TestCase
+class Environment_Test extends \PHPUnit\Framework\TestCase
 {
   /**
    * Tests PHP version
--- test/singleton_tests.php
+++ test/singleton_tests.php
@@ -7,8 +7,8 @@ require_once ('test/codebirdt.php');
  * A Twitter library in PHP.
  *
  * @package   codebird-test
- * @author    Jublo Solutions <support@jublo.net>
- * @copyright 2010-2016 Jublo Solutions <support@jublo.net>
+ * @author    Jublo Limited <support@jublo.net>
+ * @copyright 2010-2018 Jublo Limited <support@jublo.net>
  * @license   https://opensource.org/licenses/GPL-3.0 GNU General Public License 3.0
  * @link      https://github.com/jublonet/codebird-php
  */
@@ -18,7 +18,7 @@ require_once ('test/codebirdt.php');
  *
  * @package codebird-test
  */
-class Singleton_Test extends \PHPUnit_Framework_TestCase
+class Singleton_Test extends \PHPUnit\Framework\TestCase
 {
   /**
    * Tests getInstance
