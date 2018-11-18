f5e8c1743f744fca20c28c7ee2dfd294ced057f6
J.M <jm@jublo.net>
2018-11-18 17:41:09 +0100

    Update Ads API to v4

--- src/codebird.php
+++ src/codebird.php
@@ -69,8 +69,8 @@ class Codebird
    */
   protected static $_endpoints = [
     'ads'          => [
-      'production' => 'https://ads-api.twitter.com/2/',
-      'sandbox'    => 'https://ads-api-sandbox.twitter.com/2/'
+      'production' => 'https://ads-api.twitter.com/4/',
+      'sandbox'    => 'https://ads-api-sandbox.twitter.com/4/'
     ],
     'media'        => 'https://upload.twitter.com/1.1/',
     'publish'      => 'https://publish.twitter.com/',
